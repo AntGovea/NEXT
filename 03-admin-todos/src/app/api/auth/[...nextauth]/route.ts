de120fc300125c07999f9b57cf54603d4e416f64
AntGovea <teckblendxd@gmail.com>
2025-06-05 14:56:54 -0600

    refactor(auth): create structure for auth authentication

---
+++ 03-admin-todos/src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,22 @@
+import NextAuth, { NextAuthOptions } from 'next-auth'
+import GithubProvider from 'next-auth/providers/github'
+
+
+
+
+
+
+
+export const authOptions: NextAuthOptions = {
+    // Configure one or more authentication providers
+    providers: [
+        GithubProvider({
+            clientId: process.env.GITHUB_ID ?? '',
+            clientSecret: process.env.GITHUB_SECRET ?? '',
+        }),
+        // ...add more providers here
+    ],
+}
+
+const hanlder = NextAuth(authOptions);
+export { hanlder as GET, hanlder as POST }
