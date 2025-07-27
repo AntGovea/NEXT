447aa17160a268ec945c2ed83ab8afb4230fa58f
AntGovea <teckblendxd@gmail.com>
2025-07-26 19:28:44 -0600

    fix(styles):comment styles and use username and image from user login

--- 03-admin-todos/src/app/dashboard/page.tsx
+++ 03-admin-todos/src/app/dashboard/page.tsx
@@ -17,7 +17,6 @@ export default async function DashboardPage() {
     return (
         <div className="grid gap-6 grid-cols-1 sm:grid-cols-2">
 
-<<<<<<< HEAD
             <WidgetItem title='Usuario conectado S-side' children={<>
 
                 <div className="flex flex-col">
@@ -27,9 +26,6 @@ export default async function DashboardPage() {
 
                 </div>
             </>} />
-=======
-            {/* <WidgetItem title='' children={<></>}/> */}
->>>>>>> fabe85a (fix: conflicts)
 
         </div>
     )
--- 03-admin-todos/src/components/Sidebar.tsx
+++ 03-admin-todos/src/components/Sidebar.tsx
@@ -1,12 +1,11 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { CiLogout } from 'react-icons/ci';
-<<<<<<< HEAD
 import { SidebarItem } from './SideBarItem';
-=======
-import { SidebarItem } from './SidebarItem';
->>>>>>> fabe85a (fix: conflicts)
 import { IoAddCircleSharp, IoBagAddOutline, IoCalendarOutline, IoCheckboxOutline, IoListOutline } from 'react-icons/io5';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { redirect } from 'next/navigation';
 
 const menuItems = [
   {
@@ -37,7 +36,18 @@ const menuItems = [
 ]
 
 
-export const Sidebar = () => {
+export const Sidebar = async () => {
+
+
+  const session = await getServerSession(authOptions)
+
+  const avatarUrl = session?.user?.image ? session.user.image : 'image other side'
+  const userName = session?.user?.name ?? 'No Name';
+
+  if (!session) {
+    redirect('/api/auth/signin')
+  }
+
   return (
     <aside className="ml-[-100%] fixed z-10 top-0 pb-3 px-6 w-full flex flex-col justify-between h-screen border-r bg-white transition duration-300 md:w-4/12 lg:ml-0 lg:w-[25%] xl:w-[20%] 2xl:w-[15%]">
       <div>
@@ -45,9 +55,10 @@ export const Sidebar = () => {
           {/* TODO: Next/Link hacia dashboard */}
           <Link href="#" title="home">
             {/* Next/Image */}
-            <Image src="https://tailus.io/sources/blocks/stats-cards/preview/images/logo.svg" 
-              className="w-32" 
-              alt="tailus logo" 
+            <Image
+              src={avatarUrl}
+              className="w-32"
+              alt="tailus logo"
               width={150}
               height={150}
             />
@@ -55,25 +66,25 @@ export const Sidebar = () => {
         </div>
 
         <div className="mt-8 text-center">
-         
-          <Image 
-            src="https://tailus.io/sources/blocks/stats-cards/preview/images/second_user.webp" 
+
+          <Image
+            src={avatarUrl}
             width={150}
             height={150}
-            alt="" 
-            className="w-10 h-10 m-auto rounded-full object-cover lg:w-28 lg:h-28" 
+            alt=""
+            className="w-10 h-10 m-auto rounded-full object-cover lg:w-28 lg:h-28"
           />
-          <h5 className="hidden mt-4 text-xl font-semibold text-gray-600 lg:block">Cynthia J. Watts</h5>
+          <h5 className="hidden mt-4 text-xl font-semibold text-gray-600 lg:block">{userName}</h5>
           <span className="hidden text-gray-400 lg:block">Admin</span>
         </div>
 
         <ul className="space-y-2 tracking-wide mt-8">
           {
-            menuItems.map( item => (
-              <SidebarItem key={ item.path } {...item} />
+            menuItems.map(item => (
+              <SidebarItem key={item.path} {...item} />
             ))
           }
-          
+
         </ul>
       </div>
 
